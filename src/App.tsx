597e332a70ebd0b5e6ebd258e619c6d82d91fcb3
Jozias Martini <jozias.martini@gmail.com>
2023-08-14 16:27:06 -0300

    feat: start design, create new loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AccessibilityNewOutlined } from "@mui/icons-material";
+import "./App.css";
+import LinearWithValueLabel from "./components/LinearProgressWithLabel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex justify-center items-center flex-col">
+      <header className="flex flex-col justify-center items-center">
+        <span className="block mt-4 text-3xl">
+          Hey, this{" "}
+          <AccessibilityNewOutlined className="text-7xl text-green-700" /> is me
+        </span>
+        <span className="block text-neutral-500">
+          I'm waiting for you to hire me
+        </span>
       </header>
+
+      <div className="w-2/4 my-4">
+        <LinearWithValueLabel />
+      </div>
     </div>
   );
 }
---
+++ src/components/LinearProgressWithLabel.tsx
@@ -0,0 +1,54 @@
+import * as React from "react";
+import LinearProgress, {
+  LinearProgressProps,
+} from "@mui/material/LinearProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+function LinearProgressWithLabel(
+  props: LinearProgressProps & { value: number }
+) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center" }}>
+      <Box sx={{ width: "100%", mr: 1 }}>
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+      <Box sx={{ minWidth: 35 }}>
+        <Typography variant="body2" color="text.secondary">{`${Math.round(
+          props.value
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+export default function LinearWithValueLabel() {
+  const [progress, setProgress] = React.useState(10);
+
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 10 : prevProgress + 10
+      );
+    }, 800);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <LinearProgressWithLabel
+        value={progress}
+        sx={{
+          height: "10px",
+          borderRadius: "10px",
+          backgroundColor: "#e0e0e0",
+          "& .MuiLinearProgress-barColorPrimary": {
+            backgroundColor: "green",
+          },
+        }}
+      />
+    </Box>
+  );
+}
