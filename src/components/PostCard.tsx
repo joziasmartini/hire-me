2413598ff5146c1809600ec082a66daeda560028
Jozias Martini <jozias.martini@gmail.com>
2023-08-14 19:36:17 -0300

    feat: create PostCard component

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+
+interface PostCardProps {
+  image: string;
+  title: string;
+  description: string;
+}
+
+export default function PostCard({ image, title, description }: PostCardProps) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia component="img" image={image} alt="Image" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
